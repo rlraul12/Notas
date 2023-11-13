2a8c8a3a9169e6146421562c68578fc3ef4d0a79
Raul E. Paneque <rlraul12@gmail.com>
2023-11-13 14:09:29 -0500

    Initial Commit

---
+++ Notas/NotasApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by Raul Paneque on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct NotasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
